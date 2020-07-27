8e25f0cac40438f444490e133b5f355f4c847dd9
= <bhargav.annem@gmail.com>
2020-07-27 14:58:40 -0500

    NLP file

---
+++ NLP.py
@@ -0,0 +1,50 @@
+import os
+
+import numpy as np
+import tensorflow as tf
+
+import tensorflow_hub as hub
+import tensorflow_datasets as tfds
+tfds.disable_progress_bar()
+
+from official.modeling import tf_utils
+from official import nlp
+from official.nlp import bert
+
+# Load the required submodules
+import official.nlp.optimization
+import official.nlp.bert.bert_models
+import official.nlp.bert.configs
+import official.nlp.bert.run_classifier
+import official.nlp.bert.tokenization
+import official.nlp.data.classifier_data_lib
+import official.nlp.modeling.losses
+import official.nlp.modeling.models
+import official.nlp.modeling.networks
+
+gs_folder_bert = "gs://cloud-tpu-checkpoints/bert/keras_bert/uncased_L-12_H-768_A-12"
+tf.io.gfile.listdir(gs_folder_bert)
+hub_url_bert = "https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/2"
+
+glue, info = tfds.load('glue/mrpc', with_info=True,
+                       # It's small, load the whole dataset
+                       batch_size=-1)
+
+
+# Set up tokenizer to generate Tensorflow dataset
+tokenizer = bert.tokenization.FullTokenizer(
+    vocab_file=os.path.join(gs_folder_bert, "vocab.txt"),
+     do_lower_case=True)
+
+print("Vocab size:", len(tokenizer.vocab))
+
+tokens = tokenizer.tokenize("Is arson and theft a felony in Pennsylvania?")
+print(tokens)
+ids = tokenizer.convert_tokens_to_ids(tokens)
+cnt = 0;
+for ele in ids:
+          if ele > 3000:
+                    print(ele)
+                    print(tokens[cnt])
+          cnt += 1
+# print(ids)
